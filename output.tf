915e58e0c042ec1ff0ba283508c95c6f8f2ef175
angryluke <lbolli@hashicorp.com>
2023-05-12 16:54:46 +0200

    removed output.tf file

--- output.tf
+++
@@ -1,7 +0,0 @@
-output "iam-identity-provider-arn" {
-  value = aws_iam_openid_connect_provider.tfc_provider.arn
-}
-
-output "iam-identity-provider-url" {
-  value = aws_iam_openid_connect_provider.tfc_provider.url
-}
